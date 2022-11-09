b987b5c9571d36d62f17cb6fda07f3d1d75d697a
Garin Hamburg <garin.w.hamburg@gmail.com>
2022-11-08 21:36:23 -0800

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new(); 
+    
+    /*
+
+    Adding 'mut' to the variable deffinition means it can be motified.
+    Variables are immutable by default
+
+    so 
+    let apples = 5;
+    can't be edited later
+
+    but
+    let mut apples = 5;
+    can be
+
+    */
+    
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line")
+    
+    println!("You guessed: {guess}");
 }
